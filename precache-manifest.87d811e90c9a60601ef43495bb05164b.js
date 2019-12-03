0e61f335e5ff6f7d5270aade6a83f22725300ffc
Sadhan Kumar <sadhan.kumar@borngroup.com>
2019-12-03 16:54:34 +0530

    Updates

---
+++ precache-manifest.87d811e90c9a60601ef43495bb05164b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b44e8ae972d6681c55434bd1fe7bcd",
+    "url": "/monster-page/index.html"
+  },
+  {
+    "revision": "27c5b2c5affefbfa2120",
+    "url": "/monster-page/static/css/main.b7b9a800.chunk.css"
+  },
+  {
+    "revision": "e681d63e092c05d48422",
+    "url": "/monster-page/static/js/2.7699bed9.chunk.js"
+  },
+  {
+    "revision": "27c5b2c5affefbfa2120",
+    "url": "/monster-page/static/js/main.b41e53b5.chunk.js"
+  },
+  {
+    "revision": "08c084c62f5a50f7b20d",
+    "url": "/monster-page/static/js/runtime-main.70c86125.js"
+  }
+]);
